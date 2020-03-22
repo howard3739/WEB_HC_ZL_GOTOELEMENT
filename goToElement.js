aac30f540a08ae3e16afd7fa0b32b149a7f3bf0c
howard3739 <howard810116@gmail.com>
2020-03-22 13:03:08 +0800

    前往指定元素

---
+++ goToElement.js
@@ -0,0 +1,16 @@
+$("*").each(function () {
+    $(this).click(function () { 
+        // 目標屬性
+        var target = $(this).attr("data-goto-target");
+
+        // 如果 目標屬性 不等於 未定義資料
+        if (target != undefined) {
+            // 時間屬性
+            var duration = $(this).attr("data-goto-duration");
+            // 上方位置 = 目標屬性.位移.上方
+            var top = $(target).offset().top;
+            // 動畫({捲動上方：上方位置}，時間)
+            $("html, body").animate({scrollTop: top}, parseInt(duration));
+        }
+    });
+});
