45595c5ade459183aee8b1647c3ccba87894c8f2
howard3739 <howard810116@gmail.com>
2020-03-22 14:44:20 +0800

    前往指定元素

--- goToElement.js
+++ goToElement.js
@@ -9,8 +9,16 @@ $("*").each(function () {
             var duration = $(this).attr("data-goto-duration");
             // 上方位置 = 目標屬性.位移.上方
             var top = $(target).offset().top;
-            // 動畫({捲動上方：上方位置}，時間)
-            $("html, body").animate({scrollTop: top}, parseInt(duration));
+            // 先停止所有動畫 再執行動畫({捲動上方：上方位置}，時間)
+            $("html, body").stop().animate({scrollTop: top}, parseInt(duration));
         }
+    });
+});
+
+// 當捲動滾輪時停止動畫
+// jqon 事件：mousewheel 滑鼠滾動
+$(window).on("mousewheel", function () {
+    $("*").each(function () {
+        $(this).stop();
     });
 });
